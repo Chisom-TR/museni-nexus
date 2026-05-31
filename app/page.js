942c778ac9cf0425dfc2f2cf9adbd78b4bf77128
chisomgit <91879186+Chisom-TR@users.noreply.github.com>
2026-05-31 15:31:28 +0200

    Add call to action button

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 export default function Home() {
     return (
         <div>
-            <h1>Museni Nexus</h1>
+            <h1>Museni <span className="gradient-text">Nexus</span></h1>
             <p>Transforming Data into Strategic Advantage</p>
+            <a className="cta-button" href="#">Get Your Free Consultation</a>
         </div>
     );
 }
